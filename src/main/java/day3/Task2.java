c82f2209121bbf7a7cd3cd7b3659b03ebb7610c2
Andrey Dubinin <a.dubinin.88@gmail.com>
2021-02-24 15:09:20 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double x;
+        double y;
+        while (true) {
+            x = scanner.nextDouble();
+            y = scanner.nextDouble();
+            if (y != 0) {
+                System.out.println(x / y);
+                continue;
+            } else {
+                break;
+            }
+        }
     }
 }
