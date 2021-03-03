d2fbf0b6f652638e193a5a35be7117e8b70bc0a3
Andrey Dubinin <a.dubinin.88@gmail.com>
2021-03-03 13:50:39 +0300

    complete day9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,15 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Bob", "7B");
+        Teacher teacher = new Teacher("Alex", "Geography");
 
+        System.out.println(student.getGroupName());
+        System.out.println();
+        System.out.println(teacher.getSubjectName());
+        System.out.println();
+        student.printInfo();
+        System.out.println();
+        teacher.printInfo();
     }
 }
