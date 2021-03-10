df698806b80b6b4ef5219fdd4d36d6bea1a7e985
Andrey Dubinin <a.dubinin.88@gmail.com>
2021-03-10 11:55:43 +0300

    day14 task1 fixed

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -11,30 +11,29 @@ public class Task1 {
     }
 
     public static void printSumDigits(File file) {
-        Scanner scanner = null;
         try {
-            scanner = new Scanner(file);
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbersString = line.split("\\s+");
+            int sum = 0;
+
+            if (numbersString.length != 10) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else {
+                for (String number : numbersString) {
+                    sum += Integer.parseInt(number);
+                }
+            }
+            if (sum != 0) {
+                System.out.println(sum);
+            }
+            scanner.close();
+            
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Некорректный входной файл");
         }
-        String line = scanner.nextLine();
-        String[] numbersString = line.split("\\s+");
-        int sum = 0;
 
-        if (numbersString.length != 10) {
-            try {
-                throw new ArrayIndexOutOfBoundsException();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Некорректный входной файл");
-            }
-        } else {
-            for (String number : numbersString) {
-                sum += Integer.parseInt(number);
-            }
-        }
-        if (sum != 0) {
-            System.out.println(sum);
-        }
-        scanner.close();
     }
 }
