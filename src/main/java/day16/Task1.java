7d45488b2866de43fb7318e1ed6650f7c4183997
Andrey Dubinin <a.dubinin.88@gmail.com>
2021-03-11 21:10:02 +0300

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,34 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("Numbers");
+        printResult(file);
+    }
 
+    public static void printResult(File file){
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbers = line.split("\\s+");
+            double sum = 0.0;
+
+            for (String number : numbers) {
+                sum += Integer.parseInt(number);
+            }
+
+            double result = sum / numbers.length;
+
+            System.out.println(result + " --> " + new DecimalFormat("#.###").format(result));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");;
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,58 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random = new Random();
+        File file1 = new File("file1.txt");
+        try {
+            PrintWriter writer1 = new PrintWriter("file1.txt");
+            for (int i = 0; i < 1000; i++) {
+                writer1.println(random.nextInt(100));
+            }
+            writer1.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        try {
+            Scanner scanner = new Scanner(file1);
+            File file2 = new File("file2.txt");
+            PrintWriter writer2 = new PrintWriter(file2);
+            double sum = 0.0;
+            int repeat = 0;
+            while (scanner.hasNextLine()) {
+                sum += Double.parseDouble(scanner.nextLine());
+                repeat++;
+                if (repeat == 20) {
+                    writer2.println(sum / repeat);
+                    sum = 0.0;
+                    repeat = 0;
+                }
+            }
+            writer2.close();
+            scanner.close();
+            printResult(file2);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+    }
 
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            double result = 0.0;
+            while (scanner.hasNextLine()) {
+                result += Double.parseDouble(scanner.nextLine());
+            }
+            System.out.println((int) result);
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
